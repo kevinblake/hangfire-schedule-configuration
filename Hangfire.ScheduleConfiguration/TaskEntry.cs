f15675c34dbeab6a3d5017d6bb058ab8ec697cfe
Kevin Blake <kblake@puzzlebox.co.uk>
2015-03-17 18:58:35 +0000

    Initial commit

---
+++ Hangfire.ScheduleConfiguration/TaskEntry.cs
@@ -0,0 +1,39 @@
+﻿using System.Configuration;
+
+namespace Hangfire.ScheduleConfiguration
+{
+    public class TaskEntry : ConfigurationElement
+    {
+        [ConfigurationProperty("name", IsKey = true, IsRequired = true)]
+        public string Name
+        {
+            get { return (string) base["name"]; }
+
+            set { base["name"] = value; }
+        }
+
+        [ConfigurationProperty("type", IsKey = false, IsRequired = true)]
+        public string ServiceType
+        {
+            get { return (string)base["type"]; }
+
+            set { base["type"] = value; }
+        }
+
+        [ConfigurationProperty("cron", IsKey = false, IsRequired = true)]
+        public string Cron
+        {
+            get { return (string) base["cron"]; }
+
+            set { base["cron"] = value; }
+        }
+
+        [ConfigurationProperty("method", IsKey = false, IsRequired = true)]
+        public string Method
+        {
+            get { return (string) base["method"]; }
+
+            set { base["method"] = value; }
+        }
+    }
+}
---
+++ Hangfire.ScheduleConfiguration/TaskSection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Configuration;
+
+using Hangfire.Common;
+
+namespace Hangfire.ScheduleConfiguration
+{
+    public class TaskSection : ConfigurationSection
+    {
+        [ConfigurationProperty("tasks", IsRequired = true)]
+        public TaskList Tasks
+        {
+            get { return (TaskList) this["tasks"]; }
+
+            set { this["tasks"] = value; }
+        }
+
+        public void Initialise()
+        {
+            var manager = new RecurringJobManager();
+
+            foreach (TaskEntry task in this.Tasks.Items)
+            {
+                var type = Type.GetType(task.ServiceType);
+                if (type != null)
+                {
+                    var method = type.GetMethod(task.Method);
+                    var job = new Job(type, method);
+
+                    manager.AddOrUpdate(task.Name, job, task.Cron);
+                }
+            }
+        }
+    }
+}
