c9e67685a411d3c450e071a8c6b5a97f9a0d98cc
Kevin Blake <kevin.blake@boden.co.uk>
2016-07-19 14:20:30 +0100

    Fix enqueue method

--- Hangfire.ScheduleConfiguration/TaskSection.cs
+++ Hangfire.ScheduleConfiguration/TaskSection.cs
@@ -2,6 +2,7 @@
 using System.Configuration;
 using System.Linq.Expressions;
 using Hangfire.Common;
+using Hangfire.States;
 
 namespace Hangfire.ScheduleConfiguration
 {
@@ -34,7 +35,7 @@ namespace Hangfire.ScheduleConfiguration
 
         public void Enqueue()
         {
-            var server = new BackgroundJobClient(JobStorage.Current);
+            var client = new BackgroundJobClient(JobStorage.Current);
 
             foreach (TaskEntry task in this.Tasks.Items)
             {
@@ -42,12 +43,12 @@ namespace Hangfire.ScheduleConfiguration
                 if (type != null)
                 {
                     var method = type.GetMethod(task.Method);
+                    var job = new Job(type, method);
 
-                    var action = Expression.Lambda<Action>(Expression.Call(Expression.New(type), method));
-
-                    server.Enqueue(action);
+                    client.Create(job, new EnqueuedState("default"));
                 }
             }
         }
+
     }
 }
